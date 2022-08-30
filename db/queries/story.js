d2c6173281ebad59b5cfd775fb5683e86a1d3e21
paige-clark <paigeclarkillustration@gmail.com>
2022-08-30 22:55:54 +0000

    connected story entries and got the entries on screen

--- db/queries/story.js
+++ db/queries/story.js
@@ -4,10 +4,14 @@ const getStoryInfo = (storyID) => {
   const query = `
   SELECT stories.title AS story_title,
          stories.initial_story,
-         story_blocks.*
+         story_entries.entry_text,
+         story_blocks.completed,
+         story_entries.selected AS winning_entry
   FROM stories
   JOIN story_blocks
     ON stories.id = story_blocks.story_id
+  JOIN story_entries
+    ON story_blocks.id = story_entries.story_block_id
   WHERE stories.id = $1
   ;`;
 
